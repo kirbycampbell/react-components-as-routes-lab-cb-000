272615c70ddd0b1db78d864518cd839f46d4e534
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-19 18:03:18 +0000

    automatically backed up by learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,7 +5,7 @@ const Movies = () => {
   return (
     <div>
         <h1>Movies Page</h1>
-        <div>{movies.map(movie => {
+        <div>{movies.map((movie) => {
           <li>{movie.title}</li>
           <li>{movie.time}</li>
           <li>{movie.genre}</li>
