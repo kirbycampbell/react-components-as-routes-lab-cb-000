692a175dc5b543189940da8b3756993eab437d7a
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-19 18:22:23 +0000

    automatically backed up by learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -8,7 +8,7 @@ const Directors = () => {
     {directors.map((movie, index) => {
       <div key={index}>
       <li>{director.name}</li>
-      <li>{director.movie}</li>
+
         {director.movie.map((movie, index) => (
           <li key={index}>{movie}</li>
         ))}</div>
